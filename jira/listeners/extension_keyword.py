42f4ef77d03148ecace3b658ee80794577addbb6
Safa Arıman <safa.ariman@sekomyazilim.com.tr>
2018-12-12 11:57:00 +0300

    added exception handling

--- jira/listeners/extension_keyword.py
+++ jira/listeners/extension_keyword.py
@@ -2,8 +2,10 @@
 """ Created by Safa Arıman on 12.12.2018 """
 import base64
 import json
+from ulauncher.api.shared.action.DoNothingAction import DoNothingAction
 from ulauncher.api.shared.action.OpenUrlAction import OpenUrlAction
 import urllib
+import urlparse
 import urllib2
 from ulauncher.api.client.EventListener import EventListener
 from ulauncher.api.shared.action.RenderResultListAction import RenderResultListAction
@@ -18,31 +20,66 @@ class ExtensionKeywordListener(EventListener):
         self.icon_file = icon_file
 
     def on_event(self, event, extension):
-
         query = event.get_argument()
+        results = []
 
-        url = extension.preferences.get('url')
+        workspace_url = extension.preferences.get('url')
         user = extension.preferences.get('username')
         password = extension.preferences.get('password')
 
         token = base64.b64encode(str('%s:%s' % (user, password)).encode()).decode()
+        url = urlparse.urljoin(workspace_url, 'rest/internal/2/productsearch/search')
         get_url = "%s?%s" % (url, urllib.urlencode({'q': query}))
-
         req = urllib2.Request(get_url, headers={'Authorization': 'Basic %s' % token})
 
-        results = []
+        result_types = []
 
-        response = urllib2.urlopen(req)
-        result_types = json.loads(response.read())
+        try:
+            response = urllib2.urlopen(req)
+            result_types = json.loads(response.read())
+        except urllib2.HTTPError as e:
+            if e.code == 401:
+                results.append(
+                    ExtensionResultItem(
+                        name='Authentication failed.',
+                        description='Please check your username/e-mail and password.',
+                        icon=self.icon_file,
+                        on_enter=DoNothingAction()
+                    )
+                )
+            return RenderResultListAction(results)
+        except urllib2.URLError as e:
+            results.append(
+                ExtensionResultItem(
+                    name='Could not connect to Jira.',
+                    description='Please check your workspace url and make sure you are connected to the internet.',
+                    icon=self.icon_file,
+                    on_enter=DoNothingAction()
+                )
+            )
+            return RenderResultListAction(results)
+
+        print(json.dumps(result_types))
+
         for rtype in result_types:
             for item in rtype.get('items', []):
                 key = item.get('subtitle')
                 title = item.get('title')
-                res_name = rtype.get('name')
                 url = item.get('url')
                 results.append(
-                    ExtensionResultItem(name=key, description=title if not key else '%s - %s' % (key, title),
+                    ExtensionResultItem(
+                        name=title if not key else '%s - %s' % (key, title),
+                        description=key,
                         icon=self.icon_file, on_enter=OpenUrlAction(url=url))
                 )
+
+        if not results:
+            results.append(
+                ExtensionResultItem(
+                    name="Search '%s'" % query,
+                    description='No results. Try searching something else :)',
+                    icon=self.icon_file,
+                    on_enter=DoNothingAction())
+            )
 
         return RenderResultListAction(results)
